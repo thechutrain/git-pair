df0a8fa76082f8d94d10a9f3651e5aeeb3b02d2d
thechutrain <chu.chuyalan@gmail.com>
2019-06-03 13:28:23 -0400

    test lets see what happens

--- actions/status.go
+++ actions/status.go
@@ -1,24 +1,44 @@
 package actions
 
 import (
+	"bufio"
 	"fmt"
-	"io/ioutil"
+	"os"
+	"strings"
 )
 
 // Status prints out who you are currently pairing with
 func Status() {
 	// TODO: get the filename or project name
 	// NOTE: possible that the file does not exist and will throw an error
-	data, err := ioutil.ReadFile("current_pairs.txt")
-
-	// Note: assuming if there was an error opening the file, file doesnt exist
-	// perhaps there is a better way to do this?h
+	// data, err := ioutil.ReadFile("current_pairs.txt")
+	file, err := os.Open("current_pairs.txt")
 	if err != nil {
-		fmt.Println("You are not currently pairing with anyone")
+		fmt.Println("Your are not currently pairing with anyone")
+		return
 	}
 
-	pairs := string(data)
-	fmt.Printf("Pairs from with: %#v", pairs)
-	// open current_pair
-	// log each of those pairs
+	var pairs []*Collaborator
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		words := strings.Split(line, " ")
+		pairs = append(pairs, &Collaborator{GhName: words[0], Email: words[1]})
+	}
+
+
+	switch len(pairs) {
+	case 0:
+		fmt.Println("You are not currently pairing with anyone")
+	case 1: 
+		fmt.Println("You are currently pairing with 1 person:")
+		// for pair in range pairs{
+		// 	fmt.Println(:"")
+		}
+	}
+	// if len(pairs) == 0 {
+	// 	fmt.Println("Your are not currently pairing with anyone")
+	// } else if (len(pairs)) {
+	// 	fmt.Println("You are currently pairing with %d p")
+	// }
 }
